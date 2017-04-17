1bd314d6f3ddb62f6d7f8a8a0c92b5752e426e64
Mateusz Rzepa <mrzepa89@gmail.com>
2017-04-17 09:00:24 +0200

    minimize dist

--- contribution.md
+++ contribution.md
@@ -5,4 +5,8 @@ Bower requires adding tags to have package up to date:
 ```bash
 git tag -a v1.0.6  -m "Release 1.0.6"
 git push origin master --tags
+
+npm run browserify
+git tag -a v1.0.7  -m "Release 1.0.7" && git push origin master --tags 
+npm publish
 ```
---
+++ tests/browserifySpec.js
@@ -0,0 +1,186 @@
+'use strict';
+
+var should = require('should');
+var expect = require('expect');
+var assert = require('assert');
+
+describe('itemjs general tests', function() {
+
+  var items = [{
+    name: 'movie1',
+    tags: ['a', 'b', 'c', 'd'],
+    actors: ['a', 'b']
+  }, {
+    name: 'movie2',
+    tags: ['a', 'e', 'f'],
+    actors: ['a', 'b']
+  }, {
+    name: 'movie3',
+    tags: ['a', 'c'],
+    actors: ['e']
+  }]
+
+  var itemsjs = require('./../dist/itemsjs')(items);
+
+  it('makes search', function test(done) {
+    var result = itemsjs.search();
+    assert.equal(result.data.items.length, 3);
+    done();
+  });
+
+  it('makes search with pagination', function test(done) {
+    var result = itemsjs.search({
+      per_page: 1
+    });
+    assert.equal(result.data.items.length, 1);
+
+    var result = itemsjs.search({
+      per_page: 1,
+      page: 4
+    });
+    assert.equal(result.data.items.length, 0);
+
+    var result = itemsjs.search({
+      per_page: 1,
+      page: 3
+    });
+    assert.equal(result.data.items.length, 1);
+    done();
+  });
+
+  it('makes search with aggregation filters', function test(done) {
+
+    var itemsjs = require('./../dist/itemsjs')(items, {
+      aggregations: {
+        tags: {},
+        actors: {}
+      }
+    });
+
+    var result = itemsjs.search({
+      filters: {
+        tags: ['e', 'f']
+      }
+    });
+    assert.equal(result.data.items.length, 1);
+
+    var result = itemsjs.search({
+      filters: {
+        tags: ['e', 'f'],
+        actors: ['a', 'b']
+      }
+    });
+    assert.equal(result.data.items.length, 1);
+
+    var result = itemsjs.search({
+      filters: {
+        tags: ['e', 'f'],
+        actors: ['a', 'd']
+      }
+    });
+    assert.equal(result.data.items.length, 0);
+
+    var result = itemsjs.search();
+    assert.equal(result.data.items.length, 3);
+    done();
+  });
+
+  it('makes aggregations when configuration supplied', function test(done) {
+    var itemsjs = require('./../dist/itemsjs')(items, {
+      aggregations: {
+        tags: {
+          type: 'terms',
+          size: 10,
+          title: 'Tags'
+        }
+      }
+    });
+    var result = itemsjs.search({});
+    assert.equal(result.data.items.length, 3);
+    assert.equal(result.data.aggregations.tags.name, 'tags');
+    assert.equal(result.data.aggregations.tags.buckets.length, 6);
+    done();
+  });
+
+  it('makes aggregations for non array (string) fields', function test(done) {
+    var items = [{
+      name: 'movie1',
+      tags: 'a',
+    }, {
+      name: 'movie2',
+      tags: 'a',
+    }, {
+      name: 'movie3',
+      tags: 'a',
+    }];
+
+    var itemsjs = require('./../dist/itemsjs')(items, {
+      aggregations: {
+        tags: {
+          type: 'terms',
+          size: 10,
+          title: 'Tags'
+        }
+      }
+    });
+    var result = itemsjs.search({});
+    assert.equal(result.data.items.length, 3);
+    assert.equal(result.data.aggregations.tags.name, 'tags');
+    assert.equal(result.data.aggregations.tags.buckets.length, 1);
+    assert.equal(result.data.aggregations.tags.buckets[0].doc_count, 3);
+    done();
+  });
+
+  it('makes aggregations for undefined field', function test(done) {
+    var items = [{
+      name: 'movie1',
+    }, {
+      name: 'movie2',
+    }, {
+      name: 'movie3',
+    }];
+
+    var itemsjs = require('./../dist/itemsjs')(items, {
+      aggregations: {
+        tags: {
+          type: 'terms',
+          size: 10,
+          title: 'Tags'
+        }
+      }
+    });
+    var result = itemsjs.search({});
+    assert.equal(result.data.items.length, 3);
+    assert.equal(result.data.aggregations.tags.name, 'tags');
+    assert.equal(result.data.aggregations.tags.buckets.length, 0);
+    //assert.equal(result.data.aggregations.tags.buckets[0].doc_count, 3);
+    done();
+  });
+
+  it('search by tags', function test(done) {
+    var items = [{
+      name: 'movie1',
+      tags: ['drama']
+    }, {
+      name: 'movie2',
+      tags: ['drama', 'crime']
+    }, {
+      name: 'movie3',
+    }];
+
+    var itemsjs = require('./../dist/itemsjs')(items, {
+      searchableFields: ['name', 'tags']
+    });
+    var result = itemsjs.search({
+      query: 'drama'
+    });
+    assert.equal(result.data.items.length, 2);
+
+    var result = itemsjs.search({
+      query: 'crime'
+    });
+    assert.equal(result.data.items.length, 1);
+    done();
+  });
+
+});
